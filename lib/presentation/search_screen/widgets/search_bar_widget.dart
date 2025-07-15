3827bab0ba462e400b8952936ba4f28544ba3d1b
EddyOne81 <duinguyen88@gmail.com>
2025-07-15 22:53:31 +0700

    create search_bar_widget

---
+++ lib/presentation/search_screen/widgets/search_bar_widget.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+import '../../../core/app_export.dart';
+
+class SearchBarWidget extends StatelessWidget {
+  final TextEditingController controller;
+  final FocusNode focusNode;
+  final Function(String) onSubmitted;
+  final VoidCallback onVoiceSearch;
+  final VoidCallback onBarcodeSearch;
+  final bool isVoiceSearching;
+
+  const SearchBarWidget({
+    super.key,
+    required this.controller,
+    required this.focusNode,
+    required this.onSubmitted,
+    required this.onVoiceSearch,
+    required this.onBarcodeSearch,
+    required this.isVoiceSearching,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(4.w),
+      decoration: BoxDecoration(
+        color: AppTheme.lightTheme.colorScheme.surface,
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(
+          color: AppTheme.lightTheme.colorScheme.outline,
+          width: 1,
+        ),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: controller,
+              focusNode: focusNode,
+              onSubmitted: onSubmitted,
+              style: AppTheme.lightTheme.textTheme.bodyLarge,
+              decoration: InputDecoration(
+                hintText: 'Search movies, shows, actors...',
+                hintStyle: AppTheme.lightTheme.textTheme.bodyMedium?.copyWith(
+                  color: AppTheme.lightTheme.colorScheme.onSurface
+                      .withValues(alpha: 0.6),
+                ),
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(3.w),
+                  child: CustomIconWidget(
+                    iconName: 'search',
+                    color: AppTheme.lightTheme.colorScheme.onSurface
+                        .withValues(alpha: 0.6),
+                    size: 20,
+                  ),
+                ),
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 4.w,
+                  vertical: 3.w,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
