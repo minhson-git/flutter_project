4b535729e637b9165697652dbf8f6eecdd3eed2d
minhson-git <minhsont2112@gmail.com>
2025-07-15 15:19:07 +0700

    create app route

---
+++ lib/routes/app_route.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppRoutes {
+  // TODO: Add your routes here
+  static const String initial = '/';
+  static const String homeScreen = '/home-screen';
+
+
+  static Map<String, WidgetBuilder> routes = {
+    // TODO: Add your other routes here
+  };
+}
