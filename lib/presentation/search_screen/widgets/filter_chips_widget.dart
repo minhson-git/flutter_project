bc6325f86b738186fcafc5ae3b1bb62a4ddcb41a
EddyOne81 <duinguyen88@gmail.com>
2025-07-16 23:18:55 +0700

    update filter_chips_widget

--- lib/presentation/search_screen/widgets/filter_chips_widget.dart
+++ lib/presentation/search_screen/widgets/filter_chips_widget.dart
@@ -1,2 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
+
+import '../../../core/app_export.dart';
+import '../../../theme/app_theme.dart';
+
+class FilterChipsWidget extends StatelessWidget {
+  final List<String> filters;
+  final List<String> selectedFilters;
+  final Function(String) onFilterToggle;
+
+  const FilterChipsWidget({
+    super.key,
+    required this.filters,
+    required this.selectedFilters,
+    required this.onFilterToggle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 6.h,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        padding: EdgeInsets.symmetric(horizontal: 4.w),
+        itemCount: filters.length,
+        separatorBuilder: (context, index) => SizedBox(width: 2.w),
+        itemBuilder: (context, index) {
+          final filter = filters[index];
+          final isSelected = selectedFilters.contains(filter);
+
+          return GestureDetector(
+            onTap: () => onFilterToggle(filter),
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 4.w,
+                vertical: 1.h,
+              ),
+              decoration: BoxDecoration(
+                color: isSelected
+                    ? AppTheme.lightTheme.colorScheme.primary
+                    : AppTheme.lightTheme.colorScheme.surface,
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(
+                  color: isSelected
+                      ? AppTheme.lightTheme.colorScheme.primary
+                      : AppTheme.lightTheme.colorScheme.outline,
+                  width: 1,
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  filter,
+                  style: AppTheme.lightTheme.textTheme.bodySmall?.copyWith(
+                    color: isSelected
+                        ? AppTheme.lightTheme.colorScheme.onPrimary
+                        : AppTheme.lightTheme.colorScheme.onSurface,
+                    fontWeight: isSelected ? FontWeight.w600 : FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
