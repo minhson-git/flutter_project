56fe462961c31c848922c36fe166160c64913cde
minhson-git <minhsont2112@gmail.com>
2025-07-21 22:23:00 +0700

    merge branches

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -5,10 +5,12 @@ class AppRoutes {
   // TODO: Add your routes here
   static const String initial = '/';
   static const String homeScreen = '/home-screen';
-
+  static const String categoriesScreen = '/categories-screen';
+  static const String searchScreen = '/search-screen';
 
   static Map<String, WidgetBuilder> routes = {
     homeScreen: (context) => const HomeScreen(),
+
     // TODO: Add your other routes here
   };
 }
