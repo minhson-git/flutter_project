589933a6545fb0aaff80aa846388040e76d68955
EddyOne81 <duinguyen88@gmail.com>
2025-07-27 22:10:04 +0700

    update trending searches widget

---
+++ lib/presentation/search_screen/widgets/trending_searches_widget.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+import '../../../core/app_export.dart';
+
+class TrendingSearchesWidget extends StatelessWidget {
+  final List<String> trendingSearches;
+  final Function(String) onTrendingTap;
+
+  const TrendingSearchesWidget({
+    super.key,
+    required this.trendingSearches,
+    required this.onTrendingTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            CustomIconWidget(
+              iconName: 'trending_up',
+              color: AppTheme.lightTheme.colorScheme.primary,
+              size: 20,
+            ),
+            SizedBox(width: 2.w),
+            Text(
+              'Trending Searches',
+              style: AppTheme.lightTheme.textTheme.titleMedium?.copyWith(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 2.h),
+        ListView.separated(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: trendingSearches.length,
+          separatorBuilder: (context, index) => SizedBox(height: 1.h),
+          itemBuilder: (context, index) {
+            final search = trendingSearches[index];
+            return GestureDetector(
+              onTap: () => onTrendingTap(search),
+              child: Container(
+                padding: EdgeInsets.all(3.w),
+                decoration: BoxDecoration(
+                  color: AppTheme.lightTheme.colorScheme.surface,
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(
+                    color: AppTheme.lightTheme.colorScheme.outline,
+                    width: 1,
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 8.w,
+                      height: 4.h,
+                      decoration: BoxDecoration(
+                        color: AppTheme.lightTheme.colorScheme.primary
+                            .withValues(alpha: 0.1),
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: Center(
+                        child: Text(
+                          '${index + 1}',
+                          style:
+                          AppTheme.lightTheme.textTheme.bodySmall?.copyWith(
+                            color: AppTheme.lightTheme.colorScheme.primary,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 3.w),
+                    Expanded(
+                      child: Text(
+                        search,
+                        style: AppTheme.lightTheme.textTheme.bodyMedium,
+                      ),
+                    ),
+                    CustomIconWidget(
+                      iconName: 'arrow_forward_ios',
+                      color: AppTheme.lightTheme.colorScheme.onSurface
+                          .withValues(alpha: 0.4),
+                      size: 16,
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
