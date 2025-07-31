c35a2ebe4d35441b058aa25bae0b19d88b1620ab
minhson-git <minhsont2112@gmail.com>
2025-07-31 20:57:49 +0700

    add movie service

---
+++ lib/services/services.dart
@@ -0,0 +1,8 @@
+export 'firebase_service.dart';
+export 'auth_service.dart';
+export 'user_service.dart';
+export 'movie_service.dart';
+export 'category_service.dart';
+export 'watch_history_service.dart';
+export 'review_service.dart';
+export 'playlist_service.dart';
