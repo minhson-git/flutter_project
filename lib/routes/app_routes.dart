ad0cdc14e011e3a7ff0c7f8ab0b18f40cb0c9ecc
minhson-git <minhsont2112@gmail.com>
2025-07-31 21:10:46 +0700

    update app routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -3,14 +3,20 @@ import '../presentation/home_screen/home_screen.dart';
 
 class AppRoutes {
   // TODO: Add your routes here
-  static const String initial = '/';
-  static const String homeScreen = '/home-screen';
+  static const String authScreen = '/auth-screen';
   static const String categoriesScreen = '/categories-screen';
+  static const String homeScreen = '/home-screen';
   static const String searchScreen = '/search-screen';
+  static const String contentDetailScreen = '/content-detail-screen';
+  static const String profileScreen = '/profile-screen';
 
   static Map<String, WidgetBuilder> routes = {
+    authScreen: (context) => const AuthScreen(),
+    categoriesScreen: (context) => const CategoriesScreen(),
     homeScreen: (context) => const HomeScreen(),
-
+    searchScreen: (context) => const SearchScreen(),
+    contentDetailScreen: (context) => const ContentDetailScreen(),
+    profileScreen: (context) => const ProfileScreen(),
     // TODO: Add your other routes here
   };
 }
